13a035f387ce6f5ef27cf2bcb75892d72d191d1a
semi-yu <urban.manul.1919@gmail.com>
2026-05-27 00:54:06 +0900

    chore: 포매팅을 위한 라이브러리 추가 및 적용

--- canvas.py
+++ canvas.py
@@ -1,6 +1,7 @@
 import numpy as np
 from datetime import datetime
 
+
 class Color:
     def __init__(self, r, g, b):
         self.r = r
@@ -12,23 +13,19 @@ class Color:
 
 
 class Canvas:
-    def __init__(self,
-                 width: int,
-                 height: int):
-        self._filename = f"image-{datetime.now().strftime(f"%y-%m-%d %H%M%S")}.ppm"
+    def __init__(self, width: int, height: int):
+        self._filename = f"Image-{datetime.now().strftime('%y%m%d %H%M%S')}.ppm"
 
         self._width = width
         self._height = height
 
-        self._content = np.array([
-            [[0, 0, 0]] * self._width
-        ] * self._height)
+        self._content = np.array([[[0, 0, 0]] * self._width] * self._height)
 
     def write_pixel(self, row, col, color) -> None:
         self._content[col][row] = color.arrayize()
 
     def to_ppm(self):
-        with open(self._filename, 'w') as f:
+        with open(self._filename, "w") as f:
             f.write(f"P3\n{self._width} {self._height}\n255\n")
 
             for row in range(self._height):
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv("./config.env")
 
+
 class Config:
     HEIGHT = int(os.getenv("HEIGHT"))
     WIDTH = int(os.getenv("WIDTH"))
