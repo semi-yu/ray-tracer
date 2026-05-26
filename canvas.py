80db6e99c61ca3cac5c170a59f990fc0cb11687b
semi-yu <urban.manul.1919@gmail.com>
2026-05-26 15:55:41 +0900

    feat: 데이터를 버퍼에 쓰도록 바꾸고, 나중에 사진으로 변환

--- canvas.py
+++ canvas.py
@@ -1,22 +1,37 @@
 import numpy as np
 from datetime import datetime
 
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def arrayize(self):
+        return np.array([self.r, self.g, self.b])
+
 
 class Canvas:
     def __init__(self,
                  width: int,
                  height: int):
+        self._filename = f"image-{datetime.now().strftime(f"%y-%m-%d %H%M%S")}.ppm"
+
         self._width = width
         self._height = height
 
-    def save(self,
-              data: np.array,
-              filename: str=f"image-{datetime.now().strftime(f"%y-%m-%d %H%M%S")}.ppm"
-        ) -> None:
-        with open(filename, 'w') as f:
+        self._content = np.array([
+            [[0, 0, 0]] * self._width
+        ] * self._height)
+
+    def write_pixel(self, row, col, color) -> None:
+        self._content[row][col] = color.arrayize()
+
+    def to_ppm(self):
+        with open(self._filename, 'w') as f:
             f.write(f"P3\n{self._width} {self._height}\n255\n")
 
             for row in range(self._height):
                 for col in range(self._width):
-                    r, g, b = data[row][col]
+                    r, g, b = self._content[row][col]
                     f.write(f"{r} {g} {b}\n")
