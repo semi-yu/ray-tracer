e2432a673f2b536ec7606f690c7b4960fcecf9b6
semi-yu <urban.manul.1919@gmail.com>
2026-06-01 22:47:45 +0900

    feat: 퐁 라이팅에 사용되는 머터리얼과 빛 설정

---
+++ entities/light.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from image.canvas import Color
+from util.mathematics import Point, Vector
+
+from reflect import reflect
+
+
+class Light:
+    def __init__(self, position: Point, intensity: Color):
+        self._position = position
+        self._intensity = intensity
+
+    @property
+    def position(self) -> Point:
+        return self._position
+
+    @property
+    def intensity(self) -> Color:
+        return self._intensity
+
+
+def lighting(material, light, position, eye, normal) -> Color:
+    effective_color = material.color.arrayize() * light.intensity.arrayize()
+
+    diff = light.position.coord - position.coord
+    light_vector = diff / np.linalg.norm(diff)
+
+    ambient = effective_color * material.ambient
+
+    light_dot_normal = np.dot(light_vector, normal.coord)
+
+    diffuse, specular = np.array([0, 0, 0]), np.array([0, 0, 0])
+
+    if light_dot_normal >= 0:
+        diffuse = effective_color * material.diffuse * light_dot_normal
+
+        rev_light = Vector()
+        rev_light.set_coord(-light_vector)
+
+        reflect_vector = reflect(rev_light, normal)
+        reflect_dot_eye = np.dot(reflect_vector.coord, eye.coord)
+
+        if reflect_dot_eye > 0:
+            factor = np.power(reflect_dot_eye, material.shininess)
+            specular = light.intensity.arrayize() * material.specular * factor
+
+    result = Color()
+    result.set_coord(ambient + diffuse + specular)
+
+    return result
---
+++ entities/material.py
@@ -0,0 +1,40 @@
+from image.canvas import Color
+
+
+class Material:
+    def __init__(
+        self,
+        color: Color = Color(1.0, 1.0, 1.0),
+        ambient: float = 0.1,
+        diffuse: float = 0.9,
+        specular: float = 0.9,
+        shininess: float = 200.0,
+    ):
+        self._color = color
+        self._ambient = ambient
+        self._diffuse = diffuse
+        self._specular = specular
+        self._shininess = shininess
+
+    def set_color(self, color):
+        self._color = color
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
+    def ambient(self):
+        return self._ambient
+
+    @property
+    def diffuse(self):
+        return self._diffuse
+
+    @property
+    def specular(self):
+        return self._specular
+
+    @property
+    def shininess(self):
+        return self._shininess
