eceb8137f0f53cc6cd379a919668fea181d9aaa7
semi-yu <urban.manul.1919@gmail.com>
2026-06-01 22:49:11 +0900

    feat: 구에 대한 기본 머터리얼을 지정

--- entities/sphere.py
+++ entities/sphere.py
@@ -1,18 +1,25 @@
 import numpy as np
 
+from .material import Material
+
 from util.mathematics import Point
 from util.transformation import Transformation
 
 
 class Sphere:
-    def __init__(self, center: Point, radius):
+    def __init__(self, center: Point, radius: float):
         self._center = center
         self._radius = radius
 
         self._transform = Transformation()
 
+        self._material = Material()
+
     def set_transform(self, transform):
         self._transform = transform
+
+    def set_material(self, material):
+        self._material = material
 
     @property
     def transform(self) -> Transformation:
@@ -25,3 +32,7 @@ class Sphere:
     @property
     def radius(self) -> float:
         return self._radius
+
+    @property
+    def material(self) -> Material:
+        return self._material
