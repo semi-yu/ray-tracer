daa707e80a1cb7b21af37411c9ac63e31b6a1fa2
semi-yu <urban.manul.1919@gmail.com>
2026-06-09 16:54:00 +0900

    feat: Shape 인터페이스에 맞추도록 수정

---
+++ sphere.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from intersect import Intersection
+
+from entities.material import Material
+
+from util.mathematics import Vector, Point
+from util.transformation import Transformation
+
+
+class Sphere:
+    def __init__(self, center: Point = Point(0, 0, 0), radius: float = 1.0):
+        self._center = center
+        self._radius = radius
+
+        self._transformation = Transformation()
+
+        self._material = Material()
+
+    def set_transform(self, transform):
+        self._transformation = transform
+        return self
+
+    def set_material(self, material):
+        self._material = material
+        return self
+
+    def local_intersect(self, ray):
+        diff = ray.origin.coord - self.center.coord
+
+        a = np.dot(ray.direction.coord, ray.direction.coord)
+        b = 2 * np.dot(ray.direction.coord, diff)
+        c = np.dot(diff, diff) - 1
+
+        discriminant = b * b - 4 * a * c
+
+        if discriminant < 0:
+            return []
+        else:
+            return [
+                Intersection((-b - np.sqrt(discriminant)) / (2 * a), self),
+                Intersection((-b + np.sqrt(discriminant)) / (2 * a), self),
+            ]
+
+    def local_normal_at(self, point: Point) -> Vector:
+        return Vector().set_coord(point)
+
+    @property
+    def transform(self) -> Transformation:
+        return self._transformation
+
+    @property
+    def center(self) -> Point:
+        return self._center
+
+    @property
+    def radius(self) -> float:
+        return self._radius
+
+    @property
+    def material(self) -> Material:
+        return self._material
