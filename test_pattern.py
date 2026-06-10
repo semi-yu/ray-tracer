c22f8bb92ccb5dfa3037a4adfdf5338097aa38df
semi-yu <urban.manul.1919@gmail.com>
2026-06-10 15:58:36 +0900

    test: 일반화된 패턴 클래스 정상동작 확인

--- test_pattern.py
+++ test_pattern.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 from pytest import approx
 
-from pattern import StripePattern
+from pattern import Pattern, StripePattern
 
 from util.transformation import Transformation
 
@@ -30,9 +32,9 @@ def test_a_stripe_pattern_is_constant_in_y():
 
     p = StripePattern(white, black)
 
-    assert p.stripe_at(Point(0, 0, 0)).arrayize() == approx(white.arrayize())
-    assert p.stripe_at(Point(0, 1, 0)).arrayize() == approx(white.arrayize())
-    assert p.stripe_at(Point(0, 2, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0, 0, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0, 1, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0, 2, 0)).arrayize() == approx(white.arrayize())
 
 
 def test_a_stripe_pattern_is_constant_in_z():
@@ -40,9 +42,9 @@ def test_a_stripe_pattern_is_constant_in_z():
 
     p = StripePattern(white, black)
 
-    assert p.stripe_at(Point(0, 0, 0)).arrayize() == approx(white.arrayize())
-    assert p.stripe_at(Point(0, 0, 1)).arrayize() == approx(white.arrayize())
-    assert p.stripe_at(Point(0, 0, 2)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0, 0, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0, 0, 1)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0, 0, 2)).arrayize() == approx(white.arrayize())
 
 
 def test_a_stripe_pattern_is_constant_in_x():
@@ -50,13 +52,13 @@ def test_a_stripe_pattern_is_constant_in_x():
 
     p = StripePattern(white, black)
 
-    assert p.stripe_at(Point(0, 0, 0)).arrayize() == approx(white.arrayize())
-    assert p.stripe_at(Point(0.9, 0, 0)).arrayize() == approx(white.arrayize())
-    assert p.stripe_at(Point(1.0, 0, 0)).arrayize() == approx(black.arrayize())
+    assert p.pattern_at(Point(0, 0, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(0.9, 0, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(1.0, 0, 0)).arrayize() == approx(black.arrayize())
 
-    assert p.stripe_at(Point(-0.1, 0, 0)).arrayize() == approx(black.arrayize())
-    assert p.stripe_at(Point(-1.0, 0, 0)).arrayize() == approx(black.arrayize())
-    assert p.stripe_at(Point(-1.1, 0, 0)).arrayize() == approx(white.arrayize())
+    assert p.pattern_at(Point(-0.1, 0, 0)).arrayize() == approx(black.arrayize())
+    assert p.pattern_at(Point(-1.0, 0, 0)).arrayize() == approx(black.arrayize())
+    assert p.pattern_at(Point(-1.1, 0, 0)).arrayize() == approx(white.arrayize())
 
 def test_stripes_with_an_object_transformation():
     obj = Sphere() \
@@ -68,7 +70,7 @@ def test_stripes_with_an_object_transformation():
     black, white = black_and_white()
     p = StripePattern(white, black)
 
-    color = p.stripe_at_object(obj, Point(1.5, 0.0, 0.0))
+    color = p.pattern_at_object(obj, Point(1.5, 0.0, 0.0))
 
     assert color.arrayize() == approx(white.arrayize())
 
@@ -82,7 +84,7 @@ def test_stripes_with_a_pattern_transformation():
             .scale(2.0, 2.0, 2.0)
         )
     
-    color = p.stripe_at_object(obj, Point(1.5, 0.0, 0.0))
+    color = p.pattern_at_object(obj, Point(1.5, 0.0, 0.0))
 
     assert color.arrayize() == approx(white.arrayize())
 
@@ -96,6 +98,63 @@ def test_stripes_with_both_an_object_and_a_pattern_transformation():
             .translate(0.5, 0.0, 0.0)
         )
     
-    color = p.stripe_at_object(obj, Point(2.5, 0.0, 0.0))
+    color = p.pattern_at_object(obj, Point(2.5, 0.0, 0.0))
 
     assert color.arrayize() == approx(white.arrayize())
+
+def test_default_pattern_transformation():
+    p = Pattern()
+
+    assert p.transform.matrix == approx(np.identity(4))
+
+def test_assigning_a_transformation():
+    p = Pattern() \
+        .set_transform(
+            Transformation()
+            .translate(1, 2, 3)
+        )
+    
+    assert p.transform.matrix == approx(Transformation().translate(1, 2, 3).matrix)
+
+def test_a_pattern_with_an_object_transformation():
+    s = Sphere() \
+        .set_transform(
+            Transformation()
+            .scale(2.0, 2.0, 2.0)
+        )
+    
+    p = Pattern()
+    
+    c = p.pattern_at_object(s, Point(2, 3, 4))
+
+    assert c.arrayize() == approx(Color(1.0, 1.5, 2.0).arrayize())
+
+def test_a_pattern_with_a_pattern_transformation():
+    s = Sphere()
+    
+    p = Pattern() \
+        .set_transform(
+            Transformation()
+            .scale(2.0, 2.0, 2.0)
+        )
+
+    c = p.pattern_at_object(s, Point(2, 3, 4))
+
+    assert c.arrayize() == approx(Color(1.0, 1.5, 2.0).arrayize())
+
+def test_a_pattern_with_a_pattern_transformation():
+    s = Sphere() \
+        .set_transform(
+            Transformation()
+            .scale(2.0, 2.0, 2.0)
+        )
+    
+    p = Pattern() \
+        .set_transform(
+            Transformation()
+            .translate(0.5, 1.0, 1.5)
+        )
+
+    c = p.pattern_at_object(s, Point(2.5, 3.0, 3.5))
+
+    assert c.arrayize() == approx(Color(0.75, 0.5, 0.25).arrayize())
