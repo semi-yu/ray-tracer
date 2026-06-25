e332f6407a5621a1949692620ae8ce51bc1910c3
semi-yu <urban.manul.1919@gmail.com>
2026-06-25 15:54:28 +0900

    feat: 원뿔 추가

---
+++ cone.py
@@ -0,0 +1,97 @@
+import math
+import numpy as np
+
+from entities.ray import Ray
+from util.mathematics import Vector, Point
+
+from intersect import Intersection
+from shape import Shape
+
+
+def check_cap(ray, t, y):
+    x = ray.origin.x + t * ray.direction.x
+    z = ray.origin.z + t * ray.direction.z
+
+    return (x * x + z * z) <= abs(y)
+
+
+class Cone(Shape):
+    def __init__(self, minimum: float = float('-inf'), maximum: float = float('inf'), closed = False):
+        self._minimum = minimum
+        self._maximum = maximum
+        self._closed = closed
+    
+    def local_intersect(self, ray: Ray) -> list[Intersection]:
+        a = ray.direction.x**2 - ray.direction.y**2 + ray.direction.z**2
+        b = 2 * (ray.origin.x * ray.direction.x - ray.origin.y * ray.direction.y + ray.origin.z * ray.direction.z)
+        c = ray.origin.x**2 - ray.origin.y**2 + ray.origin.z**2
+        
+        xs = []
+
+        if math.isclose(a, 0.0):
+            if not math.isclose(b, 0.0):
+                t = -c / (2 * b)
+                y = ray.origin.y + t * ray.direction.y
+                if self._minimum < y < self._maximum:
+                    xs.append(Intersection(t, self))
+        else:
+            disc = b**2 - 4 * a * c
+            if disc < 0: 
+                return []
+
+            t1 = (-b - np.sqrt(disc)) / (2 * a)
+            t2 = (-b + np.sqrt(disc)) / (2 * a)
+
+            if t1 > t2: 
+                t1, t2 = t2, t1
+
+            y0 = ray.origin.y + t1 * ray.direction.y
+            if self._minimum < y0 < self._maximum:
+                xs.append(Intersection(t1, self))
+
+            y1 = ray.origin.y + t2 * ray.direction.y
+            if self._minimum < y1 < self._maximum:
+                xs.append(Intersection(t2, self))
+
+        self.intersect_caps(ray, xs)
+        return xs
+    
+    def intersect_caps(self, ray: Ray, xs: list[Intersection]) -> None:
+        if not self._closed or math.isclose(ray.direction.y, 0.0):
+            return
+
+        t = (self.minimum - ray.origin.y) / ray.direction.y
+        if check_cap(ray, t, self.minimum): xs.append(Intersection(t, self))
+
+        t = (self.maximum - ray.origin.y) / ray.direction.y
+        if check_cap(ray, t, self.maximum): xs.append(Intersection(t, self))
+
+    
+    def local_normal_at(self, point: Point) -> Vector:
+        dist = point.x**2 + point.z**2
+
+        if dist < abs(point.y) and point.y >= self.maximum - 1e-5:
+            return Vector(0, 1, 0)
+
+        if dist < abs(point.y) and point.y <= self.minimum + 1e-5:
+            return Vector(0, -1, 0)
+
+        y = np.sqrt(dist)
+        if point.y > 0: y = -y
+
+        if math.isclose(point.x, 0.0) and math.isclose(point.z, 0.0):
+            return Vector(0, 0, 0)
+
+        return Vector(point.x, y, point.z)
+
+    @property
+    def minimum(self): 
+        return self._minimum
+
+    @property
+    def maximum(self):
+        return self._maximum
+
+    @property
+    def closed(self):
+        return self._closed
---
+++ test_cone.py
@@ -0,0 +1,71 @@
+import math
+from pytest import approx
+import numpy as np
+
+from entities.ray import Ray
+from util.mathematics import Vector, Point
+from cone import Cone
+
+def test_intersecting_a_cone_with_a_ray():
+    s = Cone()
+
+    defined_xs = [
+        (Point(0, 0, -5), Vector(   0,  0, 1), 5, 5),
+        (Point(0, 0, -5), Vector(   1,  1, 1), 8.66025, 8.66025),
+        (Point(1, 1, -5), Vector(-0.5, -1, 1), 4.55006, 49.44994),
+    ]
+
+    for origin, direction, t1, t2 in defined_xs:
+        ray = Ray(origin, direction.normalize())
+
+        xs = s.local_intersect(ray)
+
+        assert len(xs) == 2
+        assert xs[0].t == approx(t1)
+        assert xs[1].t == approx(t2)
+
+def test_intersecting_a_cone_with_a_ray_parallel_to_one_of_its_halves():
+    s = Cone()
+
+    ray = Ray(
+        Point(0, 0, -1),
+        Vector(0, 1, 1).normalize()
+    )
+
+    xs = s.local_intersect(ray)
+
+    assert len(xs) == 1
+    assert xs[0].t == approx(0.35355, abs=1e-5)
+
+def test_intersecting_a_cones_end_caps():
+    s = Cone(minimum = -0.5, maximum = 0.5, closed = True)
+
+    defined_xs = [
+        (Point(0, 0,    -5), Vector(0, 1, 0), 0, ),
+        (Point(0, 0, -0.25), Vector(0, 1, 1), 2, ),
+        (Point(0, 0, -0.25), Vector(0, 1, 0), 4, ),
+    ]
+
+    for origin, direction, count in defined_xs:
+        ray = Ray(
+            origin,
+            direction.normalize()
+        )
+
+        xs = s.local_intersect(ray)
+
+        assert len(xs) == count
+    
+def test_computing_the_normal_vector_on_a_cone():
+    s = Cone()
+
+    defined_normal = [
+        (Point( 0,  0, 0), Vector( 0,           0, 0), ),
+        (Point( 1,  1, 1), Vector( 1, -np.sqrt(2), 1), ),
+        (Point(-1, -1, 0), Vector(-1,           1, 0), ),
+    ]
+    
+    for point, normal in defined_normal:
+        n = s.local_normal_at(point)
+
+        assert normal.coord == approx(n.coord)
