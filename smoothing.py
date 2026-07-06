ffdc5fefcb93b926a4c3fb354c52210859d68fe7
semi-yu <urban.manul.1919@gmail.com>
2026-07-06 15:49:37 +0900

    feat: SmoothTriangle에 대한 법선 벡터 계산 추가

--- smoothing.py
+++ smoothing.py
@@ -1,26 +1,20 @@
+from triangle import Triangle
+
 from util.mathematics import Vector, Point
 
-class SmoothTriangle:
+class SmoothTriangle(Triangle):
     def __init__(self, p1: Point, p2: Point, p3: Point, n1: Vector, n2: Vector, n3: Vector):
-        self._p1 = p1
-        self._p2 = p2
-        self._p3 = p3
+        super().__init__(p1, p2, p3)
+
         self._n1 = n1
         self._n2 = n2
         self._n3 = n3
-    
-    @property
-    def p1(self):
-        return self._p1
-    
-    @property
-    def p2(self):
-        return self._p2
-    
-    @property
-    def p3(self):
-        return self._p3
-    
+
+    def local_normal_at(self, point: Point, hit):
+        return hit.u * self.n2 + \
+               hit.v * self.n3 + \
+               (1 - hit.u - hit.v) * self.n1
+
     @property
     def n1(self):
         return self._n1
--- test_smoothing.py
+++ test_smoothing.py
@@ -1,9 +1,15 @@
 from pytest import approx
 
+from entities.ray import Ray
 from util.mathematics import Vector, Point
 
 from triangle import Triangle
 from smoothing import SmoothTriangle
+from intersect import UVIntersection
+
+from computation import prepare_computation
+
+from entities.normal import normal_at
 
 def generate_triangle():
     tri = SmoothTriangle(
@@ -22,3 +28,44 @@ def test_contructing_a_smooth_triangle():
     assert s.n1.coord == approx(Vector(0, 1, 0).coord, abs=1e-5)
     assert s.n2.coord == approx(Vector(-1, 0, 0).coord, abs=1e-5)
     assert s.n3.coord == approx(Vector(1, 0, 0).coord, abs=1e-5)
+
+def test_an_intersection_can_encapsulate_u_and_v():
+    s = Triangle(
+        Point(0, 1, 0), Point(-1, 0, 0), Point(1, 0, 0)
+    )
+
+    i = UVIntersection(3.5, s, 0.2, 0.4)
+
+    assert i.u == approx(0.2)
+    assert i.v == approx(0.4)
+
+def test_an_intersection_with_a_smooth_triangle_stores_u_and_v():
+    s = generate_triangle()
+
+    r = Ray(Point(-0.2, 0.3, -2), Vector(0, 0, 1))
+
+    xs = s.local_intersect(r)
+
+    assert xs[0].u == approx(0.45)
+    assert xs[0].v == approx(0.25)
+
+def test_a_smooth_triangle_uses_u_and_v_to_interpolate_the_normal():
+    s = generate_triangle()
+
+    i = UVIntersection(1, s, 0.45, 0.25)
+
+    n = normal_at(s, Point(0, 0, 0), i)
+
+    assert n.coord == approx(Vector(-0.5547, 0.83205, 0.0).coord)
+
+def test_preparing_the_normal_on_a_smooth_triangle():
+    s = generate_triangle()
+
+    i = UVIntersection(1, s, 0.45, 0.25)
+
+    xs = [i]
+    r = Ray(Point(-0.2, 0.3, -2), Vector(0, 0, 1))
+
+    comps = prepare_computation(i, r, xs)
+
+    assert comps.normal.coord == approx(Vector(-0.5547, 0.83205, 0.0).coord)
