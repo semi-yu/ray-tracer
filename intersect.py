3a95dbe5024159b40eb58916145d84ca17e887ee
semi-yu <urban.manul.1919@gmail.com>
2026-07-06 15:48:35 +0900

    feat: UV 값이 포함된 Intersection 클래스 추가

--- intersect.py
+++ intersect.py
@@ -23,6 +23,20 @@ class Intersection:
     def object(self):
         return self._object
 
+class UVIntersection(Intersection):
+    def __init__(self, t: float, object, u: float, v: float):
+        super().__init__(t, object)
+        self._u = u
+        self._v = v
+
+    @property
+    def u(self):
+        return self._u
+
+    @property
+    def v(self):
+        return self._v
+
 
 def intersect_world(world, ray) -> list[Intersection]:
     result = []
