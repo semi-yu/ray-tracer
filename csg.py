355946064ba9c24639ccab3c45b5ef7fdae96d9e
semi-yu <urban.manul.1919@gmail.com>
2026-07-10 13:36:25 +0900

    feat: CSG 렌더링 구현

--- csg.py
+++ csg.py
@@ -1,19 +1,68 @@
+from entities.ray import Ray
+from intersect import intersect
+from group import Group
+
+from util.transformation import Transformation
+
+def includes(a, b):
+    if isinstance(a, Group):
+        return any(includes(child, b) for child in a.shapes)
+    elif isinstance(a, Csg):
+        return includes(a.left, b) or includes(a.right, b)
+    else:
+        return a == b
 
 
 def intersection_allowed(operation, has_left_hit, in_left, in_right):
     if operation == "union":
         return (has_left_hit and not in_right) or (not has_left_hit and not in_left)
+    elif operation == "intersection":
+        return (has_left_hit and in_right) or (not has_left_hit and in_left)
+    elif operation == "difference":
+        return (has_left_hit and not in_right) or (not has_left_hit and in_left)
     
     return False
+
+def filter_intersections(csg, intersections):
+    in_left, in_right = False, False
+
+    result = []
+
+    for intersection in intersections:
+        has_left_hit = includes(csg.left, intersection.object)
+
+        if intersection_allowed(csg.operation, has_left_hit, in_left, in_right):
+            result.append(intersection)
+        
+        if has_left_hit: in_left = not in_left
+        else: in_right = not in_right
+    
+    return result
 
 class Csg:
     def __init__(self, operation: str, left, right):
         self._operation = operation
+        self._parent = None
         self._left = left
         self._right = right
 
         self._left.set_parent(self)
         self._right.set_parent(self)
+
+        self._transformation = Transformation()
+
+    def set_transform(self, transform: Transformation):
+        self._transformation = transform
+        return self
+
+    def local_intersect(self, ray: Ray):
+        left_xs = intersect(self.left, ray)
+        right_xs = intersect(self.right, ray)
+
+        result = left_xs + right_xs
+        result.sort(key = lambda e: e.t)
+
+        return filter_intersections(self, result)
 
     @property
     def operation(self):
@@ -26,4 +75,12 @@ class Csg:
     @property
     def right(self):
         return self._right
+
+    @property
+    def transform(self):
+        return self._transformation
+    
+    @property
+    def parent(self):
+        return self._parent
 
--- test_csg.py
+++ test_csg.py
@@ -1,6 +1,14 @@
+from pytest import approx
+
+from util.mathematics import Vector, Point
+from entities.ray import Ray
+
 from sphere import Sphere
 from cube import Cube
-from csg import Csg, intersection_allowed
+from csg import Csg, intersection_allowed, filter_intersections
+
+from util.transformation import Transformation
+from intersect import Intersection
 
 def test_CSG_is_created_with_an_operation_and_two_shapes():
     s1 = Sphere()
@@ -15,17 +23,82 @@ def test_CSG_is_created_with_an_operation_and_two_shapes():
     assert s2.parent == c
 
 def test_evaluating_the_rule_for_a_CSG_operation():
-    expected_result = [False, True, False, True, False, False, True, True]
-
     values = (True, False)
+
+    expected_result = [
+        False, True, False, True, False, False, True, True, # union
+        True, False, True, False, True, True, False, False, # intersection
+        False, True, False, True, True, True, False, False, # difference
+    ]
 
     idx = 0
 
-    for has_left_hit in values:
-        for in_left in values:
-            for in_right in values:
-                result = intersection_allowed("union", has_left_hit, in_left, in_right)
+    for op in ("union", "intersection", "difference"):
+        for has_left_hit in values:
+            for in_left in values:
+                for in_right in values:
+                    result = intersection_allowed(op, has_left_hit, in_left, in_right)
 
-                assert result == expected_result[idx]
+                    assert result == expected_result[idx]
+                    idx += 1
 
-                idx += 1
+def test_filtering_a_list_of_intersections():
+    s1 = Sphere()
+    s2 = Cube()
+
+    xs = [
+            Intersection(1, s1),
+            Intersection(2, s2),
+            Intersection(3, s1),
+            Intersection(4, s2),
+    ]
+    
+    expected_index = [
+        (0, 3),
+        (1, 2),
+        (0, 1),
+    ]
+    
+    for idx, op in enumerate(("union", "intersection", "difference")):
+        c = Csg(op, s1, s2)
+
+
+        result = filter_intersections(c, xs)
+
+        i1, i2 = expected_index[idx]
+
+        assert result[0] == xs[i1]
+        assert result[1] == xs[i2]
+
+def test_a_ray_misses_a_CSG_object():
+    c = Csg("union", Sphere(), Cube())
+
+    r = Ray(Point(0, 2, -5), Vector(0, 0, 1))
+
+    xs = c.local_intersect(r)
+
+    assert len(xs) == 0
+
+def test_a_ray_hits_a_CSG_object():
+    s1 = Sphere()
+    s2 = Sphere() \
+        .set_transform(
+            Transformation()
+            .translate(0, 0, 0.5)
+        )
+
+    c = Csg("union", s1, s2)
+
+    r = Ray(Point(0, 0, -5), Vector(0, 0, 1))
+
+    xs = c.local_intersect(r)
+
+    print(xs)
+
+    assert len(xs) == 2
+
+    assert xs[0].t == approx(4)
+    assert xs[0].object == s1
+
+    assert xs[1].t == approx(6.5)
+    assert xs[1].object == s2
